708972c7919807b7fe136a4c99b130863814f1a3
alucarddelta <20882097+alucarddelta@users.noreply.github.com>
2019-07-06 15:54:14 +1000

    site set up

---
+++ naviwatch_site/src/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav class="navbar is-link" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://bulma.io">
+        <i class="fas fa-paw"></i>Navi Watch
+      </a>
+
+      <a
+        role="button"
+        class="navbar-burger burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicExample"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item">Activities</a>
+
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">Settings</a>
+
+          <div class="navbar-dropdown">
+            <a class="navbar-item">Food</a>
+            <a class="navbar-item">Activites</a>
+            <a class="navbar-item">Toliet</a>
+            <hr class="navbar-divider" />
+            <a class="navbar-item">Report an issue</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
